8c043a3760500870e2c60bbea91c9a97ec06e203
Daniel Rodriguez <danydeitu18@gmail.com>
2024-06-15 12:02:58 -0300

    add map

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,27 @@ import About from './components/about/About';
 import Mailer from './components/mailer/Mailer';
 import Footer from './components/footer/Footer';
 import Top from './components/top/Top';
-import './App.css';
 
+import './App.css';
+import MapComponent from './components/MapComponent';
 const App: React.FC = () => {
   
   return (
     <Router>
       <Top />
       <Navbar />
+   
       <Routes>
       
         <Route path="/Nosotros" Component={About} />
         <Route path="/Contacto" Component={Mailer} />
         <Route path="/Redes" Component={Footer} />
-       
+        <Route path="/ubicacion" Component={MapComponent} />
       </Routes>
    
       <About/>
       <Mailer/>
+      <MapComponent/>
       <Footer/>
     </Router>
   );
---
+++ src/components/cover/Cover.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Cover.css";
+
+
+const Cover: React.FC = () => {
+  return (
+    <div className="cover-container">
+     <img src="./img/banner.jpg"/>
+      <title>MARIAN SPORT</title>
+      <title>TALLER MECANICO INTEGRAL</title> 
+      <h3>Tren delantero | Embrague | Profesional | Aire acndicionado | Frenos </h3>
+    </div>
+  );
+};
+
+export default Cover;
