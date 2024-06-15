711caf9986acc601f098c210a434a4299c8da467
Daniel Rodriguez <danydeitu18@gmail.com>
2024-06-15 12:22:14 -0300

    add adres local

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -10,7 +10,7 @@ import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 const defaultIcon = L.icon({
   iconUrl: icon,
   shadowUrl: iconShadow,
-  iconAnchor: [12, 41], // Adjust icon anchor so the icon is properly positioned
+  iconAnchor: [12, 41], // Ajusta el anclaje del icono para que esté bien posicionado
 });
 
 L.Marker.prototype.options.icon = defaultIcon;
@@ -18,15 +18,21 @@ L.Marker.prototype.options.icon = defaultIcon;
 const position: [number, number] = [-34.545486240549245, -58.71803231252258]; // Coordenadas de San Miguel Buenos Aires, Argentina
 
 const MapComponent: React.FC = () => {
+  const handleMarkerClick = () => {
+    window.open(`https://www.google.com/maps/search/?api=1&query=${position[0]},${position[1]}`, '_blank');
+  };
+
   return (
-    <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%', margin:'3px'}}>
+    <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={position}>
+      <Marker position={position} eventHandlers={{ click: handleMarkerClick }}>
         <Popup>
-          Aquí está  San Miguel , Buenos Aires, Argentina.
+          <a href={`https://www.google.com/maps/search/?api=1&query=${position[0]},${position[1]}`} target="_blank" rel="noopener noreferrer">
+            Tribulato 1825 San Miguel ,Buenos Aires, Argentina. Haz clic para ver en Google Maps.
+          </a>
         </Popup>
       </Marker>
     </MapContainer>
